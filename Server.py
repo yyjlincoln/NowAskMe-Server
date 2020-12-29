7bae07a0b84e4ac79096a7d0b3480dbe29b9c649
Lincoln Yan <yyjlincoln@gmail.com>
2020-12-29 13:56:20 +1100

    Bug Fix: Fix the bug with ResponseModule not returning **kw

--- Server.py
+++ Server.py
@@ -2,20 +2,20 @@ from flask import Flask, Blueprint
 from flask_cors import CORS
 from utils.RequestMapping import RequestMap
 from utils.AutoArguments import Arg
+from utils.ResponseModule import Res
 
 import views.auth
 import views.devtest
 
 app = Flask(__name__)
+rmap = RequestMap()
 CORS(app)
 
-
-rmap = RequestMap()
-
 @rmap.register_request('/')
-@Arg()
-def addition(hello):
-    return hello
+@Arg(a=float,b=float,c=float)
+def addition(a,b,c=1.5):
+    return Res(0, 'ok', result=(a+b)*c)
+
 
 rmap.handle_flask(app)
 
--- utils/ResponseModule.py
+++ utils/ResponseModule.py
@@ -2,27 +2,29 @@ from flask import jsonify
 
 _ExceptionDefinitions = {
     10000: 'Development mode',
-    -1 : 'Request failed.',
+    -1: 'Request failed.',
     -10001: 'Argument {argument} was not supplied.',
     -10002: 'Conversion for {argument} could not be completed.',
 }
 
 # Client-side exception bouncing
+
+
 def Res(code, message=None, **kw):
     if message:
-        return jsonify({
+        return jsonify({**{
             'code': code,
             'message': message
-        })
+        }, **kw})
     if code in _ExceptionDefinitions:
         message = _ExceptionDefinitions[code]
         for key in kw:
             # Plug the variables in
             message = message.replace('{'+key+'}', kw[key])
-        return jsonify({
-            'code':code,
-            'message':message
-        })
-    return jsonify({
-        'code':code
-    })
+        return jsonify({**{
+            'code': code,
+            'message': message
+        }, **kw})
+    return jsonify({**{
+        'code': code
+    }, **kw})
