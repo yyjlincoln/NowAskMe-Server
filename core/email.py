788c856085cf3272fc3a4fbd22034b3bc25702d2
Lincoln Yan <yyjlincoln@gmail.com>
2021-10-05 17:16:25 +1100

    Bug Fix: Fixed a mistake in the email module

--- core/email.py
+++ core/email.py
@@ -4,5 +4,5 @@ import secrets
 
 def send_login_verification(email, name):
     otp = secrets.token_hex(3).upper()
-    sendEmail(NowaskmeLoginVerification, email=email, name=name, code=otp, fromName = 'Nowaskme Accounts Verification')
+    sendEmail(NowaskmeLoginVerification, subject="Your account verification code" ,email=email, name=name, code=otp, fromName = 'Nowaskme Account Verification')
     return otp
