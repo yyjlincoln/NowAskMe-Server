9c632882d7ab1598579feac000013a64dce35cff
yyjlincoln <yyjlincoln@gmail.com>
2021-12-27 00:43:03 +1100

    Update: Removed flask-CORS which was used for debugging

--- Server.py
+++ Server.py
@@ -5,11 +5,7 @@ import views.post
 import views.config
 from Global import API, FlaskProtocolInstance
 
-from flask_cors import CORS
-
 app = FlaskProtocolInstance.app  # Exposes the Flask app instance to Gunicorn
-
-CORS(app)
 
 API.start()
 app.run(port=8080)
