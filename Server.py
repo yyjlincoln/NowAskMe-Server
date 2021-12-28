83166e2e190a01d9162ee5979338a497068afb6e
yyjlincoln <yyjlincoln@gmail.com>
2021-12-28 14:49:30 +1100

    Bug Fix: Forgot to import the views.dev module

--- Server.py
+++ Server.py
@@ -3,6 +3,7 @@ import views.auth
 import views.user
 import views.post
 import views.config
+import views.dev
 from GlobalContext import API, FlaskProtocolInstance
 
 app = FlaskProtocolInstance.app  # Exposes the Flask app instance to Gunicorn
